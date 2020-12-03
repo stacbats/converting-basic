a0fd93553c1f08ee3cd6a0f7a1d5a67ce3c8d578
Stacy Bates (stacbats) <44994840+stacbats@users.noreply.github.com>
2020-12-03 20:57:03 +0000

    Add files via upload

--- Basic_to_.asm
+++ Basic_to_.asm
@@ -14,12 +14,4 @@ enter:
 
     rts             //  
 
-// the above program in Basic
-//          x = 0
-//          a = x
-//          poke 1024+x,a:
-//          a=1
-//          poke55296+x,a
-//          x=x+1
-//          if x <> 256 then 110
-//          end
+
